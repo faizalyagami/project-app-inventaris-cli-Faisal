e53a3ecfdb1996c23b30377b2b35901c5e3f7ace
faizalyagami <faizalyagami@gmail.com>
2025-06-16 10:29:08 +0700

    fix: laporan depresiasi dan investasi

--- repository/laporan.go
+++ repository/laporan.go
@@ -1,5 +1,11 @@
 package repository
 
+import (
+	"fmt"
+	"project-app-inventaris-cli-faisal/database"
+	"project-app-inventaris-cli-faisal/model"
+)
+
 type LaporanBarang struct {
 	ID         string
 	Nama       string
@@ -19,9 +25,29 @@ func HitungTotalInvestasiDanDepresiasi() (totalInvestasi, totalDepresiasi float6
 	return 10000000, 2500000
 }
 
-func GetLaporanBarangByID(id string) LaporanBarang {
-	if id == "B001" {
-		return LaporanBarang{ID: "B001", Nama: "Laptop A", Investasi: 5000000, Depresiasi: 1000000}
+func GetLaporanBarangByID(id int) model.LaporanBarang {
+	// if id == "B001" {
+	// 	return LaporanBarang{ID: "B001", Nama: "Laptop A", Investasi: 5000000, Depresiasi: 1000000}
+	// }
+	// return LaporanBarang{}
+	var result model.LaporanBarang
+
+	query := `
+		SELECT
+			b.id,
+			b.nama_barang,
+			b.harga AS investasi,
+			ROUND((b.harga / b.umur_ekonomis) * 
+			EXTRACT(DAY FROM NOW() - b.tanggal_beli), 2) AS despresiasi
+		FROM barang b
+		WHERE b.id = $1
+	`
+
+	row := database.GetDb().QueryRow(query, id)
+	err := row.Scan(&result.ID, &result.NamaBarang, &result.Investasi, &result.Depresiasi)
+	if err != nil {
+		fmt.Println("Barang tidak ditemukan atau gagal diambil:", err)
+		return model.LaporanBarang{}
 	}
-	return LaporanBarang{}
+	return result
 }
--- service/laporan.go
+++ service/laporan.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"project-app-inventaris-cli-faisal/repository"
+	"strconv"
 	"text/tabwriter"
 )
 
@@ -34,16 +35,28 @@ func TampilkanTotalInvestasi() {
 	writer.Flush()
 }
 
-func TampilkanLaporanPerBarang(id string) {
-	laporan := repository.GetLaporanBarangByID(id)
-	if laporan.ID == "" {
-		fmt.Println("Barang tidak ditemukan.")
+func TampilkanLaporanPerBarang(idStr string) {
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		fmt.Println("ID tidak valid.")
 		return
 	}
 
-	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	fmt.Fprintln(writer, "ID\tNama\tInvestasi\tDepresiasi")
-	fmt.Fprintln(writer, "--\t----\t----------\t-----------")
-	fmt.Fprintf(writer, "%s\t%s\tRp %.2f\tRp %.2f\n", laporan.ID, laporan.Nama, laporan.Investasi, laporan.Depresiasi)
-	writer.Flush()
+	laporan := repository.GetLaporanBarangByID(id)
+	if laporan.ID == 0 {
+		fmt.Println("Barang tidak ditemukan.")
+		return
+	}
+	fmt.Printf("ID: %d\nNama: %s\nInvestasi: Rp %.2f\nDepresiasi: Rp %.2f\n", laporan.ID, laporan.NamaBarang, laporan.Investasi, laporan.Depresiasi)
+	// laporan := repository.GetLaporanBarangByID(id)
+	// if laporan.ID == "" {
+	// 	fmt.Println("Barang tidak ditemukan.")
+	// 	return
+	// }
+
+	// writer := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	// fmt.Fprintln(writer, "ID\tNama\tInvestasi\tDepresiasi")
+	// fmt.Fprintln(writer, "--\t----\t----------\t-----------")
+	// fmt.Fprintf(writer, "%s\t%s\tRp %.2f\tRp %.2f\n", laporan.ID, laporan.Nama, laporan.Investasi, laporan.Depresiasi)
+	// writer.Flush()
 }
